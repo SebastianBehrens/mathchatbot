f2650de8843f74e6d9f0650c6610058674e0b2ae
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-23 09:52:10 +0100

    feat: simplify terminal output of latex compilation

--- scripts/create_pdf.py
+++ scripts/create_pdf.py
@@ -3,5 +3,5 @@ import logging
 
 def create_pdf(path, run_id):
     file = path / f"run_{run_id}.tex"
-    system(f"pdflatex -output-directory {path} {file}")
+    system(f"pdflatex -interaction=batchmode -output-directory {path} {file}")
     logging.info("└─ .pdf file created.")
