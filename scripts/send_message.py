c092bf24e366173c4ee0222c07879b78279a5969
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-28 19:29:41 +0100

    refactor: refactor send_message (twilio)

--- scripts/send_message.py
+++ scripts/send_message.py
@@ -10,10 +10,9 @@ def send_message(
 # TODO: set up that this function returns message sid, that is stored in pickle to be checked up on.
 
     if mode == "send_exercise":
-        body = 'Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/',
-        # body: str = f'Hi {config.contact.name}, löse doch bitte folgende Aufgabe. {media_url}',
+        message: str = f'Hi {config.contact.name}, löse doch bitte folgende Aufgabe.',
     elif mode == "send_reminder":
-        body: str = "Erinnerung."
+        message: str = "Erinnerung."
     else:
         raise_txt = f"Unclear argument given to send_message: mode = {mode}"
         logging.error(raise_txt)
@@ -21,14 +20,6 @@ def send_message(
 
     logging.disable(logging.INFO)
 
-    message = client.messages.create(
-        from_='whatsapp:+19164722756',
-        body=body,
-        to=('whatsapp:'
-            f'{re.sub( "^00", "+", str(config.contact.whatsapp_contact_string))}'
-        ),
-        media_url=[media_url]
-    )
     # update interim.pickle
 
     logging.disable(logging.NOTSET)
