1deb7e0f958ed38d9bc5d84b0c989def2117b09e
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-28 19:21:48 +0100

    fix: increase resolution of png

--- scripts/create_png.py
+++ scripts/create_png.py
@@ -6,7 +6,7 @@ def create_png(path: Path, run_id: str) -> None:
     file_pdf: Path = path / f"run_{run_id}.pdf"
     file_png: Path = path / f"run_{run_id}.png"
     system(
-        f"convert -density 250 -size 150x100 -alpha remove {file_pdf} {file_png}")
+        f"convert -density 300 -size 150x100 -alpha remove {file_pdf} {file_png}")
     logging.info("└─ .png file created.")
 
 
