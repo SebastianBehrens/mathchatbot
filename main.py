edd69fd36bb17925bb86801cdb4535a66943d3bf
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-28 19:20:21 +0100

    refactor: refactor main.py

--- main.py
+++ main.py
@@ -1,58 +1,18 @@
 from scripts.check_up_on_past_exercises import check_up_on_past_exercises
-from scripts.instantiate_logger import instantiate_logger
-from scripts.initialize_folder_today import initialize_folder_today
-from scripts.generate_run_id import generate_run_id
-from scripts.load_config import load_config
-from scripts.yield_exercise_tex import yield_exercise_tex
-from scripts.fetch_exercises import fetch_exercises
-from scripts.write_tex import create_tex
-from scripts.create_pdf import create_pdf
-from scripts.create_png import create_png
-from scripts.send_message import send_message
-# from scripts.prepare_messages import prepare_messages
-from scripts.file_to_s3 import file_to_s3
-
-import logging
-from twilio.rest import Client
-import os
-
+from scripts.dispatch_messages          import dispatch_messages
+from scripts.fetch_exercises            import fetch_exercises
+from scripts.generate_files             import generate_files
+from scripts.initialize_folder_today    import initialize_folder_today
+from scripts.instantiate_logger         import instantiate_logger
+from scripts.load_config                import load_config
+from scripts.validate_config            import validate_config
 
 if __name__ == "__main__":
-
     instantiate_logger()
     config = load_config()
+    validate_config(config)
     path = initialize_folder_today()
-    uid = generate_run_id()
     # check_up_on_past_exercises(uid, config)
     exercises = fetch_exercises(config)
-    messages_to_be_sent: list = []
-    for exercise in exercises:
-        tex = yield_exercise_tex(
-                type=exercise.type,
-                topic=exercise.topic,
-                instruction=exercise.instruction,
-                math=exercise.math)
-        logging.info("Starting to create files.")
-        create_tex(tex, path, uid)
-        create_pdf(path, uid)
-        create_png(path, uid)
-        link = file_to_s3(path, uid)
-        messages_to_be_sent.append(
-            {
-            "run_id": uid,
-            "config": config,
-            "mode": "send_message",
-            "link": link
-            })
-
-        account_sid: str = os.environ["TWILIO_ACCOUNT_SID"]
-        auth_token: str = os.environ["TWILIO_AUTH_TOKEN"]
-
-        # for msg in messages_to_be_sent:
-        client = Client(account_sid, auth_token)
-        send_message(
-            run_id=uid,
-            config=config,
-            media_url=link,
-            mode="send_exercise",
-            client=client)
+    messages_to_be_sent = generate_files(exercises, path, config)
+    dispatch_messages(messages_to_be_sent, config)
