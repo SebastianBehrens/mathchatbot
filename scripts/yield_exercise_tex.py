42df8ac38844ab3627a7cf7498fbb020ba218f87
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-28 19:33:04 +0100

    fix: rename the topics from english in exercises_general to german

--- scripts/yield_exercise_tex.py
+++ scripts/yield_exercise_tex.py
@@ -1,3 +1,6 @@
+import logging
+# TODO: handle errors per notification
+# from send_message_telegram send_message_telegram
 def yield_exercise_tex(
         type: str,
         topic: str,
@@ -5,6 +8,22 @@ def yield_exercise_tex(
         math:str) -> str:
 
     lat_newline = r"\\"
+    print("\n\n\n")
+    print(topic)
+    print("\n\n\n")
+    if topic.lower() == "exponents":
+        topic = "Exponenten"
+    elif topic.lower() == "fractions":
+        topic = "Brüche"
+    elif topic.lower() == "roots":
+        topic = "Wurzeln"
+    elif topic.lower() == "parentheses":
+        topic = "Klammern"
+    else:
+        logging.error("topic not yet translated.")
+        # send_message_telegram
+
+
     tex_template = (
         r"\documentclass[preview]{standalone}"
         r"\usepackage{tcolorbox}"
