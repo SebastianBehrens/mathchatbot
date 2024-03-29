f15930e2534fe5a65a3cf99621aa9cac8f8b6dfe
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-29 17:46:33 +0100

    chore: clean up todo

--- scripts/initialize_folder_today.py
+++ scripts/initialize_folder_today.py
@@ -7,7 +7,6 @@ def initialize_folder_today() -> Path:
     folder_today: str = str(datetime.datetime.now().strftime('%Y-%m-%d'))
     path: Path = Path().cwd() / "runs" / folder_today
     path.mkdir(parents=True, exist_ok=True)
-    # TODO: remove this again
     system(f"rm -f {path / '*'}")
     logging.info("Run folder created")
     return (path)
