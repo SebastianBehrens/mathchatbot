be96ce26e4188e15fae8316a84f7c0161e451a41
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-29 17:47:54 +0100

    refactor: refactor load_config refactor it to work with path passed as argument in addition to path being parsed from the command line args

--- scripts/load_config.py
+++ scripts/load_config.py
@@ -1,29 +1,34 @@
-from munch import DefaultMunch 
+from munch import DefaultMunch
 from pathlib import Path
 import argparse
 import logging
 import yaml
 
-def load_config() -> dict:
+def load_config(config_path) -> dict:
     # with command line arguments to config
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('conf_file',
-                        type=str,
-                        nargs='?',
-                        default='configs/valid/testing.yaml')
+    if config_path is None:
+        # parse
+        parser = argparse.ArgumentParser()
+        parser.add_argument('conf_file',
+                            type=str,
+                            nargs='?',
+                            default='configs/valid/testing.yaml')
 
-    conf_file: Path = Path().cwd() / parser.parse_args().conf_file
+        conf_file: Path = Path().cwd() / parser.parse_args().conf_file
 
-    if conf_file is None:
-        raise "Config missing. It should be the first argument given to main.py."
+        if conf_file is None:
+            raise "Config missing. It should be the first argument given to main.py."
+    else:
+        conf_file: Path = Path().cwd() / config_path
+
     with open(conf_file,'r',encoding='utf8') as file:
         config: dict = DefaultMunch.fromDict(yaml.safe_load(file))
-    
+
+    # add path of config to config, to then later on update that config
     config["self"] = conf_file
 
-
-    file: str = str(conf_file).split("/")[-1]
-    logging.info(f"Config loaded: {file}.")
+    # log only name_of_config
+    logging.info(f"Config loaded: {str(conf_file).split("/")[-1]}.")
 
     return(config)
