26dd14f7068719bc5d7bbf8323c3fb89c7c4b61f
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-29 17:48:18 +0100

    refactor: add context manager on pdf file

--- scripts/pdf_to_png.py
+++ scripts/pdf_to_png.py
@@ -1,7 +1,7 @@
 import fitz
 def pdf_to_png(file_path):
-    doc = fitz.open(file_path)
-    page = doc.load_page(0)
-    pixmap = page.get_pixmap(dpi=300)
-    img = pixmap.tobytes()
+    with fitz.open(file_path) as doc:
+        page = doc.load_page(0)
+        pixmap = page.get_pixmap(dpi=300)
+        img = pixmap.tobytes()
     return(img)
