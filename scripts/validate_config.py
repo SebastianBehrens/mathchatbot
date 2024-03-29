5c2f06ea6188dd9bbe16bc1f8b91f62aaae8a231
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-29 17:49:47 +0100

    test: update wording of error in validate_config and add validation of batch_size

--- scripts/validate_config.py
+++ scripts/validate_config.py
@@ -1,11 +1,20 @@
 from munch import DefaultMunch
+import logging
 import yaml
 from pathlib import Path
 def validate_config(config):
+
     # validate max_level
     for topic in config.topics.keys():
         with open(Path().cwd() / "exercises" / "exercises_general.yaml",'r',encoding='utf8') as file:
                 content: dict = DefaultMunch.fromDict(yaml.safe_load(file))
     if config.topics[topic].max_level != 'all':
         if config.topics[topic].max_level > len(content[topic].keys()):
-            raise ValueError(f"Topic level is not defined. The entered maximum level for topic '{topic}' is {config.topics[topic].max_level}, but there are only {len(content[topic].keys())} levels available.")
+
+            msg = f"Maximum level  of topic '{topic}' is misspecified. Level suppplied is {config.topics[topic].max_level}. Only levels up to and including {len(content[topic].keys())} are valid."
+
+            logging.exception(msg)
+            raise ValueError(msg)
+
+    if type(config.batch_size) != int:
+         raise ValueError(f"Batch size must be an integer.")
