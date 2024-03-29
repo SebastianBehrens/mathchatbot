19301685f1b28e3f35eac14c9e1c52fc3dadd085
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-03-29 13:03:31 +0100

    refactor: refactor generate_files to prepare_messages

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from scripts.check_up_on_past_exercises import check_up_on_past_exercises
 from scripts.dispatch_messages          import dispatch_messages
 from scripts.fetch_exercises            import fetch_exercises
-from scripts.generate_files             import generate_files
+from scripts.prepare_messages             import prepare_messages
 from scripts.initialize_folder_today    import initialize_folder_today
 from scripts.instantiate_logger         import instantiate_logger
 from scripts.load_config                import load_config
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     path = initialize_folder_today()
     # check_up_on_past_exercises(uid, config)
     exercises = fetch_exercises(config)
-    messages_to_be_sent = generate_files(exercises, path, config)
+    messages_to_be_sent = prepare_messages(exercises, path, config)
     dispatch_messages(messages_to_be_sent, config)
---
+++ scripts/prepare_messages.py
@@ -0,0 +1,26 @@
+import logging
+from scripts.yield_exercise_tex import yield_exercise_tex
+from scripts.tex_to_pdf import tex_to_pdf
+from datetime import datetime
+
+# REVIEW: separation of steps - prepare_messages for creating tex and pdf?
+def prepare_messages(exercises, path, config):
+    messages_to_be_sent: list = []
+    for exercise in exercises:
+        # uid = generate_run_id()
+        uid = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+        tex = yield_exercise_tex(
+                type=exercise.type,
+                topic=exercise.topic,
+                instruction=exercise.instruction,
+                math=exercise.math)
+        pdf_file = tex_to_pdf(tex, path, uid)
+
+        messages_to_be_sent.append(
+            {
+            "run_id": uid,
+            "config": config,
+            "mode": "send_message",
+            "pdf_path": pdf_file
+            })
+    return(messages_to_be_sent)
