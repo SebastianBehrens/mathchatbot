9c8262bab7df6a31a1d26f2553cf3e9adcc2badf
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-01 21:58:55 +0200

    feat: handle bad response from online latex compilation

--- scripts/tex_to_pdf.py
+++ scripts/tex_to_pdf.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import requests
+import logging
 from pathlib import Path
 
 def tex_to_pdf(tex, path, run_id):
@@ -7,6 +8,11 @@ def tex_to_pdf(tex, path, run_id):
     tex_str_enc: urllib = urllib.parse.quote(tex, safe='')
     url: str = f"https://latexonline.cc/compile?text={tex_str_enc}"
     response: requests = requests.get(url = url)
+    if response.status_code == 400:
+        logging.error("Error occured in tex_to_pdf. No pdf was created.")
+        logging.info(f"└─ run_id: {run_id}")
+        logging.info(f"└─ url: {url}")
+        raise Exception("Error occured in tex_to_pdf. No pdf was created.")
     with open(file, 'wb') as f:
         f.write(response.content)
     return(file)
