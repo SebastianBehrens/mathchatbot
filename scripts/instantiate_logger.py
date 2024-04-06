f9ed41ea5643bf53dcb979425906e2b4d95e2f77
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-06 20:29:50 +0200

    feat: update logger to log to file and to console

--- scripts/instantiate_logger.py
+++ scripts/instantiate_logger.py
@@ -1,15 +1,20 @@
 import logging
 import datetime
 from pathlib import Path
+import sys
 
 def instantiate_logger() -> None:
 
     suffix: str = str(datetime.datetime.now().strftime('%Y-%m-%d'))
     file_path: Path = Path().cwd() / "logs" / f"{suffix}.log"
 
+    file_handler = logging.FileHandler(filename=file_path)
+    console_handler = logging.StreamHandler(stream=sys.stdout)
+    handlers = [file_handler, console_handler]
+
     logging.basicConfig(
-        filename=file_path,
         level=logging.INFO,
+        handlers=handlers,
         format='%(asctime)s - %(levelname)s - %(message)s')
     logger = logging.getLogger("MathChatBot")
 
