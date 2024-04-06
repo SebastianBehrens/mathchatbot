107ae96a6d6f8ec6ee368b52f5a6debeee2d5483
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-06 15:33:02 +0200

    refactor: refactor get_configs to allow directory with configs as input

--- scripts/get_configs.py
+++ scripts/get_configs.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-def get_configs() -> None:
-    pathlist = Path().cwd().glob('configs/valid/*.yaml')
-    pathlist_rel = [
-        path.relative_to(Path().cwd()) for path in pathlist]
+def get_configs(folder='configs/valid/') -> None:
+    file_regex = f"{folder}/*.yaml".replace("//", "/")
+    pathlist = Path().cwd().glob(folder)
+    pathlist_rel = [path.relative_to(Path().cwd()) for path in pathlist]
 
     list_of_configs = [str(path) for path in pathlist_rel]
     return(list_of_configs)
