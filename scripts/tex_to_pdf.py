c0135650296ffee68701fe92aa26df136c2282b3
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-06 15:34:57 +0200

    logs: add logging

--- scripts/tex_to_pdf.py
+++ scripts/tex_to_pdf.py
@@ -5,14 +5,18 @@ from pathlib import Path
 
 def tex_to_pdf(tex, path, run_id):
     file = path / f"run_{run_id}.pdf"
+    logging.info("Fetching pdf.")
     tex_str_enc: urllib = urllib.parse.quote(tex, safe='')
     url: str = f"https://latexonline.cc/compile?text={tex_str_enc}"
-    response: requests = requests.get(url = url)
+    response: requests = requests.get(url = url, timeout = 5)
     if response.status_code == 400:
         logging.error("Error occured in tex_to_pdf. No pdf was created.")
         logging.info(f"└─ run_id: {run_id}")
         logging.info(f"└─ url: {url}")
+        logging.error(f"└─ Response: {response.text}")
+        logging.error("Program exited because of Exception.")
         raise Exception("Error occured in tex_to_pdf. No pdf was created.")
     with open(file, 'wb') as f:
         f.write(response.content)
+    logging.info("Saved pdf.")
     return(file)
