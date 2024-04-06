8053c83583e960f970cfcd411be9eea69c806a27
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-06 15:35:40 +0200

    chore: clean up

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ from scripts.get_configs import get_configs
 if __name__ == "__main__":
     # main(config_path="configs/valid/testing.yaml")
     configs = get_configs()
-    print(configs)
     for conf in configs:
         MathChatBot(config_path=conf)
