1c62b209fedb15e50098be5bda1b2601bb09342f
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-06 15:55:46 +0200

    feat: raise exception on empty config directory

--- scripts/get_configs.py
+++ scripts/get_configs.py
@@ -1,11 +1,17 @@
 from pathlib import Path
+import logging
 
 def get_configs(folder='configs/valid/') -> None:
     file_regex = f"{folder}/*.yaml".replace("//", "/")
-    pathlist = Path().cwd().glob(folder)
+    pathlist = Path().cwd().glob(file_regex)
     pathlist_rel = [path.relative_to(Path().cwd()) for path in pathlist]
 
     list_of_configs = [str(path) for path in pathlist_rel]
+
+    if len(list_of_configs) == 0:
+        logging.info(f"No configs in directory {folder}. Program exited.")
+        raise Exception(f"No configs in directory {folder}. Program exited.")
+
     return(list_of_configs)
 
     
