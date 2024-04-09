a8349262d11c63cd8964338d092da4058ed3a15b
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-04-09 17:56:08 +0200

    fix: fix imports

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from scripts.MathChatBot import MathChatBot
-from scripts.get_configs import get_configs
+from scripts.MathChatBot        import MathChatBot
+from scripts.get_configs        import get_configs
+from scripts.instantiate_logger import instantiate_logger
+
 if __name__ == "__main__":
 
+    instantiate_logger()
     configs = get_configs()
     for conf in configs:
         MathChatBot(config_path=conf)
