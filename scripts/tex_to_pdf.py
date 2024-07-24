d8feea70449b17414bf41a5ae989aa9c7ba8ce54
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-07-24 20:46:07 +0200

    chore: fix typo

--- scripts/tex_to_pdf.py
+++ scripts/tex_to_pdf.py
@@ -10,12 +10,12 @@ def tex_to_pdf(tex, path, run_id):
     url: str = f"https://latexonline.cc/compile?text={tex_str_enc}"
     response: requests = requests.get(url = url, timeout = 5)
     if response.status_code == 400:
-        logging.error("Error occured in tex_to_pdf. No pdf was created.")
+        logging.error("Error occurred in tex_to_pdf. No pdf was created.")
         logging.info(f"└─ run_id: {run_id}")
         logging.info(f"└─ url: {url}")
         logging.error(f"└─ Response: {response.text}")
         logging.error("Program exited because of Exception.")
-        raise Exception("Error occured in tex_to_pdf. No pdf was created.")
+        raise Exception("Error occurred in tex_to_pdf. No pdf was created.")
     with open(file, 'wb') as f:
         f.write(response.content)
     logging.info("Saved pdf.")
