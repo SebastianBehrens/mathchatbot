d28283073deda4f86bfd78c14436cf8c0ce4af1c
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-07-25 20:17:50 +0000

    feat: add creation of log folder if not exists

--- scripts/instantiate_logger.py
+++ scripts/instantiate_logger.py
@@ -2,11 +2,18 @@ import logging
 import datetime
 from pathlib import Path
 import sys
+from scripts.get_base_dir import get_base_dir
 
 def instantiate_logger() -> None:
 
     suffix: str = str(datetime.datetime.now().strftime('%Y-%m-%d'))
     file_path: Path = get_base_dir() / "logs" / f"{suffix}.log"
+
+    # validate
+
+    logs_folder = get_base_dir() / "logs"
+    if not logs_folder.exists():
+        logs_folder.mkdir()
 
     file_handler = logging.FileHandler(filename=file_path)
     console_handler = logging.StreamHandler(stream=sys.stdout)
