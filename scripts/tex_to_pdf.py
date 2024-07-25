bb529ef01adcb8254413d94f224e89f4acfc59ca
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-07-25 20:20:57 +0000

    fix: modify request error ending condition

--- scripts/tex_to_pdf.py
+++ scripts/tex_to_pdf.py
@@ -8,8 +8,8 @@ def tex_to_pdf(tex, path, run_id):
     logging.info("Fetching pdf.")
     tex_str_enc: urllib = urllib.parse.quote(tex, safe='')
     url: str = f"https://latexonline.cc/compile?text={tex_str_enc}"
-    response: requests = requests.get(url = url, timeout = 15)
-    if response.status_code == 400:
+    response: requests = requests.get(url = url, timeout = 60)
+    if response.status_code != 200:
         logging.error("Error occured in tex_to_pdf. No pdf was created.")
         logging.info(f"└─ run_id: {run_id}")
         logging.info(f"└─ url: {url}")
