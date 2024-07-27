647d9756fd0ba9f5b756b15b4d625f42a634f687
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-07-27 23:24:02 +0000

    feat: add logger level in initialization

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 from scripts.MathChatBot import MathChatBot
 from scripts.get_configs import get_configs
 from scripts.instantiate_logger import instantiate_logger
@@ -5,7 +6,7 @@ from scripts.instantiate_logger import instantiate_logger
 
 if __name__ == "__main__":
 
-    instantiate_logger()
+    instantiate_logger(level=logging.INFO)
     configs = get_configs()
     for conf in configs:
         MathChatBot(config_path=conf)
