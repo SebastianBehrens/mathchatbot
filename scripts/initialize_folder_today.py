345838e669f49c9f10bb96e3cf704533423a36de
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2024-07-27 23:26:27 +0000

    refactor: improve logic
    previously "folder created" was issued even though not necessarily a
    folder was created. the added dir check improves the logic.

--- scripts/initialize_folder_today.py
+++ scripts/initialize_folder_today.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from os import system
 import logging
 from scripts.get_base_dir import get_base_dir
+from scripts.run_command import run_command
 
 def initialize_folder_today() -> Path:
     """Initialize the runtime folder of the day.
@@ -15,9 +16,16 @@ def initialize_folder_today() -> Path:
     """
 
     folder_today: str = str(datetime.datetime.now().strftime('%Y-%m-%d'))
+
     path: Path = get_base_dir() / "runs" / folder_today
-    path.mkdir(parents=True, exist_ok=True)
-    system(f"rm -f {path / '*'}")
-    logging.info("Run folder created.")
+
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+        logging.info("Run folder created.")
+
+    else:
+        # clean up daily folder
+        run_command(['rm', '-f', f'{path / "*"}'])
+
     return (path)
 
